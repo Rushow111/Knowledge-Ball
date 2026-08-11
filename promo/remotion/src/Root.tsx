c1fdb043ea71760ba4327256b1d3235574a671ba
Rushow <gc11119242530@gmail.com>
2026-08-11 20:57:19 +0800

    promo: use audio-enabled composition

--- promo/remotion/src/Root.tsx
+++ promo/remotion/src/Root.tsx
@@ -1,13 +1,13 @@
 import React from 'react';
 import {Composition} from 'remotion';
-import {KnowledgeBallPromo} from './KnowledgeBallPromo';
+import {KnowledgeBallWithAudio} from './KnowledgeBallWithAudio';
 
 export const RemotionRoot: React.FC = () => {
   return (
     <>
       <Composition
         id="KnowledgeBall"
-        component={KnowledgeBallPromo}
+        component={KnowledgeBallWithAudio}
         durationInFrames={720}
         fps={30}
         width={1280}
