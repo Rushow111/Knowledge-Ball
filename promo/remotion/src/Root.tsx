6762440fb6444d0d052cfeb1244cacd2e3ff9a16
Rushow <gc11119242530@gmail.com>
2026-08-12 04:57:28 +0800

    Register five-minute Knowledge Ball composition

--- promo/remotion/src/Root.tsx
+++ promo/remotion/src/Root.tsx
@@ -1,6 +1,7 @@
 import React from 'react';
 import {Composition} from 'remotion';
 import {KnowledgeBallPromo} from './KnowledgeBallPromo';
+import {KnowledgeBallLong} from './KnowledgeBallLong';
 
 export const RemotionRoot: React.FC = () => {
   return (
@@ -9,6 +10,14 @@ export const RemotionRoot: React.FC = () => {
         id="KnowledgeBall"
         component={KnowledgeBallPromo}
         durationInFrames={720}
+        fps={30}
+        width={1280}
+        height={720}
+      />
+      <Composition
+        id="KnowledgeBall5Min"
+        component={KnowledgeBallLong}
+        durationInFrames={9000}
         fps={30}
         width={1280}
         height={720}
