c3baf927c798a080b4801e6116f5e2f352e9bfea
Rushow <gc11119242530@gmail.com>
2026-08-14 23:03:40 +0800

    Register Remotion UI stills

--- promo/remotion/src/Root.tsx
+++ promo/remotion/src/Root.tsx
@@ -1,6 +1,13 @@
 import React from 'react';
-import {Composition} from 'remotion';
+import {Composition, Still} from 'remotion';
 import {KnowledgeBallPromo} from './KnowledgeBallPromo';
+import {
+  KnowledgeBallUIExplore,
+  KnowledgeBallUINode,
+  KnowledgeBallUICreate,
+  KnowledgeBallUIVote,
+  KnowledgeBallUILogin,
+} from './KnowledgeBallUI';
 
 export const RemotionRoot: React.FC = () => {
   return (
@@ -13,6 +20,11 @@ export const RemotionRoot: React.FC = () => {
         width={1280}
         height={720}
       />
+      <Still id="KnowledgeBallUIExplore" component={KnowledgeBallUIExplore} width={1280} height={720} />
+      <Still id="KnowledgeBallUINode" component={KnowledgeBallUINode} width={1280} height={720} />
+      <Still id="KnowledgeBallUICreate" component={KnowledgeBallUICreate} width={1280} height={720} />
+      <Still id="KnowledgeBallUIVote" component={KnowledgeBallUIVote} width={1280} height={720} />
+      <Still id="KnowledgeBallUILogin" component={KnowledgeBallUILogin} width={1280} height={720} />
     </>
   );
 };
